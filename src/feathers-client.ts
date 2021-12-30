1c119bd45557b85b024f29eaa632d58c865828a3
muhammadyusuf <muhammadyusuf.kurbonov2002@gmail.com>
2021-12-31 02:46:08 +0500

    added rest transport

--- src/feathers-client.ts
+++ src/feathers-client.ts
@@ -6,7 +6,7 @@ import { iff, discard } from "feathers-hooks-common";
 import feathersVuex from "feathers-vuex";
 
 const socket = io(process.env.VUE_APP_BACKEND_HOST + ":3030", {
-  transports: ["websocket"],
+  transports: ["rest", "websocket"],
 });
 
 console.log("connection to server", process.env.VUE_APP_BACKEND_HOST);
